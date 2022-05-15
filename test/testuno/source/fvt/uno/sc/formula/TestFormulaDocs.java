216a3ac98471c8fb8490b9590b986571f64b12f7
cbmarcum <carl.marcum@codebuilders.net>
2022-05-15 17:04:19 -0400

    Refs i117960 - removed tests added during a cherry pick because the documents are not yet available.

--- test/testuno/source/fvt/uno/sc/formula/TestFormulaDocs.java
+++ test/testuno/source/fvt/uno/sc/formula/TestFormulaDocs.java
@@ -76,9 +76,6 @@ public class TestFormulaDocs {
 	@Test
 	public void testFormulaDocs() throws Exception {
 		testOneDoc( "uno/sc/fvt/FormulaTest1.ods");
-		testOneDoc( "uno/sc/fvt/StarBasicYearMonthDateHourMinuteSecondTests.ods");
-		testOneDoc( "uno/sc/fvt/StarBasicCLng.ods");
-		testOneDoc( "uno/sc/fvt/DGET on formulas.ods");
 		testOneDoc( "uno/sc/fvt/Basic Line as variable and Line Input.ods");
 	}
 
